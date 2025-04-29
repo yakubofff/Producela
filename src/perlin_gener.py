b43031087dfe5afe4b3588c394f7b77f12361c33
Makksich <113855806+Makksich@users.noreply.github.com>
2025-04-29 20:21:19 +0300

    Added ability to actualy listen for user's input

--- src/perlin_gener.py
+++ src/perlin_gener.py
@@ -7,7 +7,7 @@ def map_fin(n_of_chunks, length, height):
     noise_3 = PerlinNoise(octaves=10, seed=randint(1, 10*9))
     noise_4 = PerlinNoise(octaves=10, seed=randint(1, 10*9))
     noise_5 = PerlinNoise(octaves=10, seed=randint(1, 10*9))
-    xpix, ypix = 100, 100
+    xpix, ypix = n_of_chunks*length, n_of_chunks*length
 
     map = [[noise([i/xpix, j/ypix]) for j in range(xpix)] for i in range(ypix)]
     map_2 = [[noise_2([i/xpix, j/ypix]) for j in range(xpix)] for i in range(ypix)]
@@ -15,6 +15,6 @@ def map_fin(n_of_chunks, length, height):
     map_4 = [[noise_4([i/xpix, j/ypix]) for j in range(xpix)] for i in range(ypix)]
     map_5 = [[noise_5([i/xpix, j/ypix]) for j in range(xpix)] for i in range(ypix)]
 
-    mapa_fin = [[(map[i][j]+map_2[i][j]+map_3[i][j]+map_4[i][j]+map_5[i][j])/5 * 100 for j in range(xpix)] for i in range(ypix)]
+    mapa_fin = [[(map[i][j]+map_2[i][j]+map_3[i][j]+map_4[i][j]+map_5[i][j])/5 * height for j in range(xpix)] for i in range(ypix)]
 
     return mapa_fin
